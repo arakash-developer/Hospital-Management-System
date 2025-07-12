d5f6f47f44322cdc76feb918c1435c8a6a9a898f
akash100a <arakash968786@gmail.com>
2025-07-12 11:35:27 +0600

    d

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Window {
-  electronAPI: {
-    minimize: () => Promise<void>;
-    maximize: () => Promise<void>;
-    close: () => Promise<void>;
-  };
-}
