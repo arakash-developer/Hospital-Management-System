06404847bb31c102ec9b6b7b3bf7e56ac9c6b8a9
akash100a <arakash968786@gmail.com>
2025-07-12 13:51:16 +0600

    refactor: update Sidebar component to render logo and improve structure

--- src/component/main/Sidebar.tsx
+++ src/component/main/Sidebar.tsx
@@ -1,7 +1,13 @@
+import Logo from "@public/logo.webp";
 import React from "react";
-
 const Sidebar: React.FC = () => {
-  return <div className="w-[235px] bg-[var(--white)]">Sidebar cc</div>;
+  return (
+    <div className="w-[235px] bg-[var(--white)]">
+      <div className="mt-[30px] flex justify-center items-center">
+        <img src={Logo} alt="Logo " />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
