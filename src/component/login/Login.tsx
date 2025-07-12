e074557af3956aceb40cb8f357261dcf751e4263
akash100a <arakash968786@gmail.com>
2025-07-12 14:41:40 +0600

    refactor: improve layout and styling for Login, Sidebar, and MainLayout components

--- src/component/login/Login.tsx
+++ src/component/login/Login.tsx
@@ -2,13 +2,13 @@ import LoginImg from "@public/login.webp";
 import React from "react";
 const Login: React.FC = () => {
   return (
-    <div className="bg-[var(--white)] py-6 pl-[56px] pr-6 flex  flex-col justify-center items-center md:flex-row md:justify-between">
+    <div className="bg-[var(--white)] py-6 pl-5 md:pl-[56px] md:pr-6 flex  flex-col justify-center items-center md:flex-row md:justify-between">
       <div className="">
         <h1>Log In</h1>
         <p>Log in to securely access your dashboard</p>
       </div>
-      <div className="img_field w-[564px] h-[465px] overflow-hidden">
-        <img src={LoginImg} alt={"Login Image"} />
+      <div className="img_field xl:w-[564px] xl:h-[465px] overflow-hidden scale-[0.5] xl:scale-1">
+        <img className="w-full h-full " src={LoginImg} alt={"Login Image"} />
       </div>
     </div>
   );
--- src/component/main/Sidebar.tsx
+++ src/component/main/Sidebar.tsx
@@ -2,7 +2,7 @@ import Logo from "@public/logo.webp";
 import React from "react";
 const Sidebar: React.FC = () => {
   return (
-    <div className="w-[235px] h-full bg-[var(--white)]">
+    <div className="md:w-[235px] h-full bg-[var(--white)]">
       <div className="mt-[30px] flex justify-center items-center">
         <img src={Logo} alt="Logo " />
       </div>
