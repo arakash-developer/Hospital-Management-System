35d9ea5bf7a8dbe24b0c2315b939a0816412b2d6
akash100a <arakash968786@gmail.com>
2025-07-12 14:08:05 +0600

    refactor: enhance layout structure and styling for Login, Sidebar, and MainLayout components

--- src/component/login/Login.tsx
+++ src/component/login/Login.tsx
@@ -2,12 +2,12 @@ import LoginImg from "@public/login.webp";
 import React from "react";
 const Login: React.FC = () => {
   return (
-    <section className="py-6 pl-[56px] pr-6 flex justify-between items-center">
-      <div className="content">xx</div>
-      <div className="img_field w-[564px] h-[4665px] overflow-hidden">
+    <div className="bg-[var(--white)] py-6 pl-[56px] pr-6 flex justify-between items-center">
+      <div className="">xx</div>
+      <div className="img_field w-[564px] h-[465px] overflow-hidden">
         <img src={LoginImg} alt={"Login Image"} />
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div className="w-full flex bg-[var(--background)]">
+    <div className="w-full h-full flex items-start gap-x-8 bg-[var(--background)]">
       <Sidebar />
-      <div className="w-full">
+      <div className="w-full h-full mr-[62px]">
         <Navbar />
         <Outlet />
       </div>
