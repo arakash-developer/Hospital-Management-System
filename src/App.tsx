b6dc127f66fca34035c6e3a7846c3a633ca7f59c
akash100a <arakash968786@gmail.com>
2025-07-22 16:26:54 +0600

    dzvd

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { routes } from "@router/Router.tsx";
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-
-const App: React.FC = () => {
-  return <RouterProvider router={routes} />;
-};
-
-export default App;
