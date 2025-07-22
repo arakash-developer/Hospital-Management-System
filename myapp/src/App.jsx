ec490574e47c94967f7dfb85b96f5ed4872d6c93
akash100a <arakash968786@gmail.com>
2025-07-22 16:28:35 +0600

    h

--- myapp/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { routes } from "../src/router/";
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-
-const App = () => {
-  return <RouterProvider router={routes} />;
-};
-
-export default App;
